1aabe3f2e649da9ee8f7bb62a74cb0085ea1a83b
wolfram74 <phaugen@gmail.com>
2017-08-26 13:56:30 -0600

    renders lists properly

---
+++ docs/chap_0/equations.js
@@ -0,0 +1,26 @@
+var equations = [
+  {
+    equation:"\\oint_s \\vec D \\cdot \\vec n da = 0",
+    id:"I.0",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+  {
+    equation:"NA",
+    id:"I.0",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+]
+
+/*
+  {
+    equation:"NA",
+    id:"I.0",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+*/
---
+++ docs/scripts/runner.js
@@ -0,0 +1,36 @@
+console.log('runner loaded')
+MathJax.Hub.Config({
+    tex2jax: {inlineMath: [["$&","&$"]]}
+  });
+
+Vue.component('equation', {
+  props:['equation', 'id', 'bookPage','label','description'],
+  template:`
+  <div class='equation-block'>
+    <h4>{{id}}</h4>
+    <span>$&{{equation}}&$</span>
+    <p>
+      <span>page {{bookPage}}</span>
+      <span>{{label}}</span>
+      <span>{{description}}</span>
+    </p>
+  </div>
+  `,
+  // data: function(){return equations[0]}
+})
+
+Vue.component('equation-list',{
+  template:`
+  <div class='equation-list'>
+    <equation v-for='(equation, index) in equations'
+      v-bind:equation='equation.equation'
+      v-bind:id='equation.id'
+      v-bind:bookPage='equation.bookPage'
+      v-bind:description='equation.description'
+      v-bind:key='equation.id'>
+    </equation>
+  </div>
+  `,
+  data:function(){ return {equations:equations}}
+})
+const list = new Vue({el:'#mainList'})
