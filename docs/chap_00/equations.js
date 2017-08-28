4f7dbc0ded884cfa2fa707e1df60029d70bc490b
wolfram74 <phaugen@gmail.com>
2017-08-27 20:16:15 -0600

    more of introduction

--- docs/chap_00/equations.js
+++ docs/chap_00/equations.js
@@ -56,81 +56,93 @@ var equations = [
   },
 
   {
-    equation:`NA`,
+    equation:`V(r) = \\hbar c \\frac{Z_1Z_2\\alpha}{r}
+    [
+    1+\\frac{2\\alpha}{3\\pi}\\int^\\infty_{2m}d\\kappa
+    \\frac{\\sqrt{\\kappa^2-4m^2}}{\kappa^2}(1+\\frac{2m^2}{\\kappa^2})e^{-\\kappa r}
+    ]`,
     id:"00.006",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"12",
+    label:"QED correction to coulomb potential",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\tilde V (Q^2) = \\frac{4\\pi Z_1 Z_2 \\alpha(Q^2)}{Q^2}`,
     id:"00.007",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"12",
+    label:"Spatial Fourier Transform of interaction Energy",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`[\\alpha(Q^2)]^{-1}\\approx \\frac{1}{\\alpha(0)} - \\frac{1}{3\\pi} ln( \\frac{Q^2}{m^2e^{5/3}})`,
     id:"00.008",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"12",
+    label:"Reciprocal running coupling constant",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    D_\\alpha = \\epsilon_0E_\\alpha+(P_\\alpha - \\Sigma_\\beta \\frac{\\partial Q'_{\\alpha\\beta}}{\\partial x_\\beta}+...)\\\\
+    H_\\alpha = \\frac{1}{\\mu_0}E_\\alpha+(M_\\alpha +...)
+    `,//'
     id:"00.009",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"13",
+    label:"Electric displacement D and magnetic field H",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    D_\\alpha = \\Sigma_\\beta \\epsilon_{\\alpha\\beta}E_\\beta \\\\
+    H_\\alpha = \\Sigma_\\beta \\mu'_{\\alpha\\beta}B_\\beta
+    `,//'
     id:"00.010",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"14",
+    label:"Linear response form",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`D_\\alpha(\\vec k, \\omega) = \\Sigma_\\beta \\epsilon_{\\alpha\\beta}(\\vec k, \\omega)D_\\alpha(\\vec k, \\omega)`,
     id:"00.011",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"15",
+    label:"Electric displacement expressed as interpolation of permeability and field.",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`D_\\alpha= \\Sigma_\beta \\epsilon_{\\alpha\\beta}^{(1)} E_\\beta
+    + \\Sigma_{\\beta,\\gamma}\\epsilon_{\\alpha\\beta\\gamma}^{(2)}E_\\beta E_\\gamma + ...`,
     id:"00.012",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"16",
+    label:"Example of non-linear terms in high field media",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\oint_S \\vec D \\cdot \\vec n da = \\int_V \\rho d^3x`,
     id:"00.013",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"16",
+    label:"Integral form of Gausses law",
     description:"NA",
   },
 
   {
     equation:"\\oint_S \\vec D \\cdot \\vec n da = 0",
     id:"00.014",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"16",
+    label:"No monopole equation",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\oint_c \\vec H \\cdot d\\vec l =
+    \\int_S'[\\vec J + \\frac{\\partial \\vec D}{\\partial t}]\\cdot \\vec n' da`,
     id:"00.015",
-    bookPage:"NA",
+    bookPage:"17",
     label:"NA",
     description:"NA",
   },
@@ -138,7 +150,7 @@ var equations = [
   {
     equation:`NA`,
     id:"00.016",
-    bookPage:"NA",
+    bookPage:"17",
     label:"NA",
     description:"NA",
   },
